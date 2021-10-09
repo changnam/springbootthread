22d805f60cb423c206ba9d485f1160ab4f78ba97
changnam go <changnam.go@gmail.com>
2021-10-10 00:39:46 +0900

    controller 안에서 exception 처리하는 메소드를 별도 생성. 해당 controller 에서 발생하는 모든 exception 을 처리함

--- src/main/java/com/honsoft/controller/BlogController.java
+++ src/main/java/com/honsoft/controller/BlogController.java
@@ -2,9 +2,12 @@ package com.honsoft.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.honsoft.entity.Blog;
 import com.honsoft.exception.BlogAlreadyExistsException;
 import com.honsoft.exception.BlogNotFoundException;
+import com.honsoft.exception.handler.BlogExceptionHandler;
 import com.honsoft.service.BlogService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class BlogController {
+	private static Logger logger = LoggerFactory.getLogger(BlogController.class);
     private BlogService blogService;
     
     @Autowired
@@ -36,12 +41,13 @@ public class BlogController {
         return new ResponseEntity<List>((List) blogService.getAllBlogs(), HttpStatus.OK);
     }
     @GetMapping("blog/{id}")
-    public ResponseEntity getBlogById(@PathVariable("id") int id) {
-    	 try{
-    		    return new ResponseEntity(blogService.getBlogById(id), HttpStatus.OK);
-    		 }
-    		catch(BlogNotFoundException blogNotFoundException ){
-    		  return new ResponseEntity(blogNotFoundException.getMessage(), HttpStatus.CONFLICT);
-    		 }
+    public ResponseEntity getBlogById(@PathVariable("id") int id) throws BlogNotFoundException {
+        return new ResponseEntity(blogService.getBlogById(id), HttpStatus.OK);
+    }
+    
+    @ExceptionHandler(value = BlogAlreadyExistsException.class)
+    public ResponseEntity handleBlogAlreadyExistsException(BlogAlreadyExistsException blogAlreadyExistsException) {
+        logger.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		return new ResponseEntity("Blog already exists", HttpStatus.CONFLICT);
     }
 }
